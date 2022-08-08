2cc6bfc527a976c116aa67b5db4204c4015065c0
ericagong <48196721+ericagong@users.noreply.github.com>
2022-08-08 17:11:15 +0900

    styled-components(8/8)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Text = styled.span`
+  font-size: 50px;
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: ${(props) => props.theme.backgroundColor};
+  ${Text} {
+    &:hover {
+      font-size: 50px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box>
+        <Text>hi</Text>
+      </Box>
+      <Text>hello</Text>
+    </Father>
   );
 }
 
